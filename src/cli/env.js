25c8d64d855fbe8254fb36b9eae9dae564a0eac1
Tanya Kostromitskaia <kostromitskayatanya@gmail.com>
2025-10-26 22:39:57 +0100

    feat: resolve cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
-const parseEnv = () => {
-  // Write your code here
+export const parseEnv = () => {
+  const entries = Object.entries(process.env);
+  const rssVars = entries
+    .filter(([key]) => key.startsWith('RSS_'))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+
+  console.log(rssVars);
 };
 
 parseEnv();
